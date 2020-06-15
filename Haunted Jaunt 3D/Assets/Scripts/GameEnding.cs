c3e48fb737a7e8fe906a0845ac909cb5af824254
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-15 10:56:03 -0400

    Added UI and game end trigger

---
+++ Haunted Jaunt 3D/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameEnding : MonoBehaviour
+    {
+        private bool m_IsPlayerAtExit;
+        private float m_Timer;
+
+        public float FadeDuration = 1f;
+        public float DisplayImageDuration = 5f;
+        public CanvasGroup ExitBackgroundImageCanvasGroup;
+        public GameObject Player;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (m_IsPlayerAtExit)
+            {
+                EndLevel();
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject == Player)
+            {
+                m_IsPlayerAtExit = true;
+            }
+        }
+
+        private void EndLevel()
+        {
+            m_Timer += Time.deltaTime;
+            ExitBackgroundImageCanvasGroup.alpha = m_Timer / FadeDuration;
+
+            if (m_Timer > FadeDuration + DisplayImageDuration)
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
